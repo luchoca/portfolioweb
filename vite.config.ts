d9227c463ebf62493262e9c3698058e4623e6800
luchoca <luchocastro.3@gmail.com>
2025-09-24 00:51:29 -0300

    Add .nojekyll and fix base path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolioweb/', // Nombre de tu repositorio en GitHub
+  base: process.env.NODE_ENV === 'production' ? '/portfolioweb/' : '/',
   server: {
     port: 3000,
     open: true
